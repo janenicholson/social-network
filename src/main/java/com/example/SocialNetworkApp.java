c00d3ff508f72c512f363a18307b92e2b3731537
Jane Nicholson <jane.nicholson@masabi.com>
2018-06-01 13:04:10 +0100

    failing test for basic parse

--- src/main/java/com/example/SocialNetworkApp.java
+++ src/main/java/com/example/SocialNetworkApp.java
@@ -1,11 +1,21 @@
 package com.example;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import lombok.RequiredArgsConstructor;
+
 /**
  * Hello world!
  *
  */
+@RequiredArgsConstructor
 public class SocialNetworkApp 
 {
+    private final InputStream in;
+    private final OutputStream out;
+    private final Parser parser;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- src/test/java/com/example/SocialNetworkAppTest.java
+++ src/test/java/com/example/SocialNetworkAppTest.java
@@ -1,20 +1,30 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
-/**
- * Unit test for simple App.
- */
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class SocialNetworkAppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    @Rule public MockitoRule mocks = MockitoJUnit.rule();
+    @Mock private Parser parser;
+
+    private InputStream in;
+    private OutputStream out;
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void parse_single_input_line()
     {
-        assertTrue( true );
+        in = SocialNetworkAppTest.class.getResourceAsStream("alice_input.txt");
+        new SocialNetworkApp(in, out, parser).main(null);
+        verify(parser).parse("Alice /post What a wonderfully sunny day!");
     }
 }
