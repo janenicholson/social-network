8cdaac0a39328f76e3300818deb720e60bdca4f3
Jane Nicholson <jane.nicholson@masabi.com>
2018-06-01 15:44:13 +0100

    allow parameter-less commands

--- src/main/java/com/example/Parser.java
+++ src/main/java/com/example/Parser.java
@@ -12,7 +12,7 @@ public class Parser {
     private final CommandService commandService;
 
     public void parse(String userInput) {
-        Matcher parsedInput = Pattern.compile("(\\S+)\\s+/(\\S+)\\s+(.*)").matcher(userInput);
+        Matcher parsedInput = Pattern.compile("(\\S+)\\s+/(\\S+)\\s*(.*)").matcher(userInput);
         parsedInput.matches();
         userService.createUser(parsedInput.group(1));
         commandService.executeCommand(parsedInput.group(2), parsedInput.group(3));
