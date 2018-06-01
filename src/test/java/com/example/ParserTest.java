c6f84e1d844a70e7c42a2cd7e7439e096ec77ee2
Jane Nicholson <jane.nicholson@masabi.com>
2018-06-01 15:32:07 +0100

    refactor

--- src/test/java/com/example/ParserTest.java
+++ src/test/java/com/example/ParserTest.java
@@ -17,16 +17,19 @@ public class ParserTest {
 
     @Test
     public void create_user_Alice_when_first_seen() {
-        new Parser(userService, commandService).parse("Alice /do something");
+        parser().parse("Alice /do something");
         verify(userService).createUser("Alice");
         verifyNoMoreInteractions(userService);
     }
 
     @Test
     public void executes_do_something_when_called() {
-        new Parser(userService, commandService).parse("Alice /do something");
+        parser().parse("Alice /do something");
         verify(commandService).executeCommand("do", "something");
         verifyNoMoreInteractions(commandService);
     }
 
+    private Parser parser() {
+        return new Parser(userService, commandService);
+    }
 }
